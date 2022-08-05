4b906762741c59fe28888358c9d1504b4037634a
lei1996 <617218165@qq.com>
2022-08-05 09:51:37 +0000

    update

--- packages/a/src/index.ts
+++ packages/a/src/index.ts
@@ -1,3 +1,9 @@
 import { a } from "@pnpm-test/core";
+import { Big, BigSource } from "@pnpm-test/core/big";
+import { from } from "@pnpm-test/core/rxjs";
 
 console.log(a);
+
+from([1, 2, 3]).subscribe(console.log);
+
+console.log(new Big(1).times(0.5).toNumber());
